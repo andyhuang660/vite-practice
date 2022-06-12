6e089afcdaab6f62170bc9025bfca0db7b7f75c3
andyhuang660 <kevin6071020@gmail.com>
2022-06-12 21:04:17 +0800

    add file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import TopImage from './components/TopImage.vue'
+export default {
+  components: {
+    TopImage
+  }
+}
+</script>
+
+<template lang="pug">
+#app
+  #header
+    TopImage
+  #left
+  #right
+  #footer
+</template>
+
---
+++ src/components/TopImage.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+img(src="../assets/logo2.png")
+</template>
